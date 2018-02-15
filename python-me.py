8708b2f47fba9d619ec239a93499dd3f6a613cbe
Tristan <t.siebers@exonet.nl>
2018-02-15 15:56:35 +0100

    Add Python Examples

---
+++ python-me.py
@@ -0,0 +1,22 @@
+import json
+try:
+    from urllib.request import Request, urlopen  # Python 3
+except ImportError:
+    from urllib2 import Request, urlopen  # Python 2
+
+endpoint = '/me'
+
+try:
+    request = Request('https://api.exonet.nl/' + endpoint.lstrip('/'))
+    request.add_header("Authorization", "Bearer " + open('./api-token.txt', 'r').read())
+    request.add_header("Accept", "application/vnd.Exonet.v1+json")
+    
+    content = json.loads(urlopen(request).read().decode('utf-8'))
+
+    print("")
+    print("Hello " + content['data']['attributes']['name'] + "!")
+    print("")
+    print("-" * 10 + "[ Full decoded response ]" + "-" * 10)
+    print(json.dumps(content, indent=4))
+except:
+    print('HTTP Request failed')
---
+++ requests-me.py
@@ -0,0 +1,27 @@
+# Install the Python Requests library:
+# `pip install requests`
+
+import requests
+import json
+
+endpoint = '/me'
+
+try:
+    response = requests.get(
+        url="https://api.exonet.nl/" + endpoint.lstrip('/'),
+        headers={
+            "Authorization": "Bearer " + open('./api-token.txt', 'r').read(),
+            "Accept": "application/vnd.Exonet.v1+json",
+        },
+    )
+    content = json.loads(response.content.decode('utf-8'))
+
+    print("")
+    print("Hello " + content['data']['attributes']['name'] + "!")
+    print("")
+    print("-" * 10 + "[ Full decoded response ]" + "-" * 10)
+    print(json.dumps(content, indent=4))
+
+except requests.exceptions.RequestException:
+    print('HTTP Request failed')
+
