86a36dcb8439225b6a37a6d2a10847b8a3eb37b9
Tristan <t.siebers@exonet.nl>
2018-02-15 15:56:18 +0100

    Add PHP Examples

---
+++ curl-me.php
@@ -0,0 +1,38 @@
+<?php
+
+// The API Endpoint to access.
+$endpoint = '/me';
+
+// The API token as provided by Exonet.
+$token = file_get_contents(__DIR__.'/api-token.txt');
+
+// Create the cURL resource.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, sprintf('https://api.exonet.nl/%s', ltrim($endpoint, '/')));
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Authorization: Bearer '.$token,
+    'Accept: application/vnd.Exonet.v1+json',
+]);
+
+// Send the request and store the response.
+$apiResponse = curl_exec($ch);
+
+if (!$apiResponse) {
+    echo sprintf('Error: "%s" - Code: %d', curl_error($ch), curl_errno($ch));
+} else {
+    $decodedResponse = json_decode($apiResponse);
+    echo "\n";
+    echo sprintf('Hello %s!', $decodedResponse->data->attributes->name);
+    echo "\n\n";
+    echo str_repeat('-', 10).'[ Full decoded response ]'.str_repeat('-', 10);
+    echo "\n\n";
+
+    print_r($decodedResponse);
+}
+
+// Close request to clear up some resources
+curl_close($ch);
+
+echo "\n";
---
+++ guzzle-me.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * Install the Guzzle library:
+ * `composer require guzzlehttp/guzzle:~6.0`
+ */
+
+require __DIR__.'/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\ClientException;
+
+// The API Endpoint to access.
+$endpoint = '/me';
+
+// The API token as provided by Exonet.
+$token = file_get_contents(__DIR__.'/api-token.txt');
+
+// Create the client and the request.
+$client = new Client();
+$request = new Request(
+    'GET',
+    sprintf('https://api.exonet.nl/%s', ltrim($endpoint, '/')),
+    [
+        'Authorization' => 'Bearer '.$token,
+        'Accept' => 'application/vnd.Exonet.v1+json'
+    ]);
+
+// Send the request and parse the response.
+try {
+    $apiResponse = $client->send($request);
+    $decodedResponse = json_decode($apiResponse->getBody());
+    echo "\n";
+    echo sprintf('Hello %s!', $decodedResponse->data->attributes->name);
+    echo "\n\n";
+    echo str_repeat('-', 10).'[ Full decoded response ]'.str_repeat('-', 10);
+    echo "\n\n";
+
+    print_r($decodedResponse);
+} catch (ClientException $clientException) {
+    echo $clientException->getMessage();
+}
+
+
+echo "\n";
